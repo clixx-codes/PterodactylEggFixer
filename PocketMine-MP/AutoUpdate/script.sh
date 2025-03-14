49a34c62aeaeca0059c57a65ee93a9854c813c6e
lubro <142106680+lokiPM@users.noreply.github.com>
2025-03-14 16:31:48 +0100

    Update script.sh

--- PocketMine-MP/AutoUpdate/script.sh
+++ PocketMine-MP/AutoUpdate/script.sh
@@ -1,10 +1,24 @@
-version=$(wget -qO- https://raw.githubusercontent.com/lokiPM/PterodactylEggFixer/refs/heads/main/PocketMine-MP/version)
+#!/bin/bash
+
+version=$(wget -qO- https://raw.githubusercontent.com/lokiPM/PterodactylEggFixer/main/PocketMine-MP/version)
+
+if [ -z "$version" ]; then
+    echo "Fehler: Die Version konnte nicht geladen werden."
+    exit 1
+fi
 
 if [ -f "PocketMine-MP.phar" ]; then
     rm -f PocketMine-MP.phar
 fi
 
 wget "https://github.com/pmmp/PocketMine-MP/releases/download/$version/PocketMine-MP.phar"
-echo "PocketMine-MP File installed!"
-cd
+
+if [ $? -eq 0 ]; then
+    echo "PocketMine-MP.phar installation done!"
+else
+    echo "Error"
+    exit 1
+fi
+
+cd ~
 ./bin/php7/bin/php ./PocketMine-MP.phar --no-wizard --disable-ansi
